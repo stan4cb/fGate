f8770d79e76364f4bf20ebac01b549be5c303b8e
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-06-02 17:38:08 +0300

    init

---
+++ fGate/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace fGate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(this.textBox8, "textBox8");
+            this.textBox8.Name = "textBox8";
+            this.textBox8.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox8_KeyPress);
+            this.textBox8.Leave += new System.EventHandler(this.textBox8_Leave);
+            // 
+            // textBox9
+            // 
+            resources.ApplyResources(this.textBox9, "textBox9");
+            this.textBox9.Name = "textBox9";
+            this.textBox9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox9_KeyPress);
+            this.textBox9.Leave += new System.EventHandler(this.textBox9_Leave);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ fGate/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Globalization;
+
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace fGate
+{
+    public partial class Form1 : Form
+    {
+        int x, y; // Begin Offsets
+        int playTime = 0;
+        int settingsMenuOffset = 170; // Menu expended +size
+        short printTimes;
+        string PrinterName = " - ";
+        DateTime beginTime, endTime;
+        Size defaultFormSize;
+        PrintDocument pDocument; // Main Document to print
+
+        public Form1() // Inıt + Load Settings
+        {
+            InitializeComponent();
+
+            defaultFormSize = this.Size;
+            beginTime = System.DateTime.Now;
+
+            LoadSettings();
+            UpdateTexts();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) // Save Settings
+        {
+            SaveSettings();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Print Button
+        {
+            if (textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "")
+                PrintDocument();
+            else
+                MessageBox.Show("Eksik kısımları doldurun.");
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Open-Close Advanced Settings
+        {
+            if (this.Size == defaultFormSize)
+            {
+                button2.Text = "<";
+                this.Size = new Size(this.Size.Width + settingsMenuOffset, this.Size.Height);
+                button2.Location = new Point(button2.Location.X + settingsMenuOffset, button2.Location.Y);
+            }
+            else
+            {
+                button2.Text = ">";
+                this.Size = defaultFormSize;
+                button2.Location = new Point(button2.Location.X - settingsMenuOffset, button2.Location.Y);
+            }
+
+            if (textBox2.Text == "Stanislav" && textBox3.Text == "Ursache")
+                MessageBox.Show("Welcome Master!");
+        }
+
+        private void button3_Click(object sender, EventArgs e) // TopInfo Edit Mode
+        {
+            bool val = !textBox1.ReadOnly;
+            textBox1.ReadOnly = val;
+
+            if (val)
+                button3.BackColor = Color.Red;
+            else
+                button3.BackColor = Color.Blue;
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Get Current Time
+        {
+            beginTime = System.DateTime.Now;
+            UpdateTexts();
+        }
+
+        private void button5_Click(object sender, EventArgs e)// BottomInfo Edit Mode
+        {
+            bool val = !textBox7.ReadOnly;
+            textBox7.ReadOnly = val;
+            if (val)
+                button5.BackColor = Color.Red;
+            else
+                button5.BackColor = Color.Blue;
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Select Printer
+        {
+            PrintDialog pD = new PrintDialog();
+            pD.UseEXDialog = true;
+            DialogResult dR = pD.ShowDialog();
+            if (dR == DialogResult.OK)
+            {
+                PrinterName = pD.PrinterSettings.PrinterName;
+                printTimes = pD.PrinterSettings.Copies;
+                UpdateTexts();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Dec Play Time
+        {
+            if (playTime > 0)
+                playTime -= 30;
+            UpdateTexts();
+        }
+
+        private void button8_Click(object sender, EventArgs e) // Inc Play Time
+        {
+            playTime += 30;
+            UpdateTexts();
+        }
+
+        private void button9_Click(object sender, EventArgs e) // Show contact info
+        {
+            MessageBox.Show("Herhangi bir sorun yada düzeletme için :\n d34dkn16h7@gmail ile iletişime geçiniz.");
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e) // Make Title1
+        {
+            if (!String.IsNullOrEmpty(textBox2.Text))
+            {
+                TextInfo t = new CultureInfo("tr-TR", false).TextInfo;
+                textBox2.Text = t.ToTitleCase(textBox2.Text);
+            }
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e) // Make Title2
+        {
+            if (!String.IsNullOrEmpty(textBox3.Text))
+            {
+                TextInfo t = new CultureInfo("tr-TR", false).TextInfo;
+                textBox3.Text = t.ToTitleCase(textBox3.Text);
+            }
+        }
+
+        private void textBox8_Leave(object sender, EventArgs e) // Make x Offset
+        {
+            int.TryParse(textBox8.Text, out x);
+        }
+
+        private void textBox9_Leave(object sender, EventArgs e) // Make y Offset
+        {
+            int.TryParse(textBox9.Text, out y);
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e) // Make Numeric Veli Num
+        {
+            const char Delete = (char)8;
+            e.Handled = !Char.IsDigit(e.KeyChar) && e.KeyChar != Delete;
+        }
+
+        private void textBox8_KeyPress(object sender, KeyPressEventArgs e) // Make Numeric x offset
+        {
+            const char Delete = (char)8;
+            e.Handled = !Char.IsDigit(e.KeyChar) && e.KeyChar != Delete;
+        }
+
+        private void textBox9_KeyPress(object sender, KeyPressEventArgs e)// Make Numeric y offset
+        {
+            const char Delete = (char)8;
+            e.Handled = !Char.IsDigit(e.KeyChar) && e.KeyChar != Delete;
+        }
+
+        void UpdateTexts() // Update Play Time Label
+        {
+            label8.Text = "Oyun Süresi : " + playTime.ToString() + " dk";
+            textBox5.Text = beginTime.ToShortTimeString();
+            endTime = beginTime;
+            endTime = endTime.AddMinutes(playTime);
+            textBox6.Text = endTime.ToShortTimeString();
+            label9.Text = "Aktif Yazıcı :\n" + PrinterName;
+            textBox8.Text = x.ToString();
+            textBox9.Text = y.ToString();
+        }
+
+        void PrintDocument() // Start Printing + is Print Valid
+        {
+            if (PrinterName == " - ")
+                button6_Click(null, null);
+
+            pDocument = new PrintDocument();
+
+            pDocument.PrinterSettings.PrinterName = PrinterName;
+            pDocument.PrinterSettings.Copies = printTimes;
+            pDocument.PrintPage += pDocument_PrintPage;
+
+            if (pDocument.PrinterSettings.IsValid)
+                pDocument.Print();
+            else
+                button6_Click(null, null);
+        }
+
+        void pDocument_PrintPage(object sender, PrintPageEventArgs e) // Print Document
+        {
+            int ySpaceOffset = 25;
+            Font fArial8 = new Font("arial", 9);
+            Font fArial16 = new Font("arial", 17);
+            Font fArial18 = new Font("arial", 19);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            Graphics graphics = e.Graphics;
+
+            // Üst Bilgi
+            graphics.DrawString("   " + textBox1.Text, fArial18, brush, new Point(x, y + ySpaceOffset / 2));
+            // Çocuk İsmi
+            graphics.DrawString(label2.Text + " " + textBox2.Text, fArial16, brush, new Point(x, y + ySpaceOffset * 2));
+            // Veli İsmi
+            graphics.DrawString(label3.Text + "     " + textBox3.Text, fArial16, brush, new Point(x, y + ySpaceOffset * 3));
+            // Veli Numarası
+            graphics.DrawString(label4.Text + "      " + textBox4.Text, fArial16, brush, new Point(x, y + ySpaceOffset * 4));
+            // Giriş Saati
+            graphics.DrawString(label5.Text + " " + textBox5.Text, fArial16, brush, new Point(x, y + ySpaceOffset * 5));
+            // Bitiş Saati
+            graphics.DrawString(label6.Text + "  " + textBox6.Text, fArial16, brush, new Point(x, y + ySpaceOffset * 6));
+            // Alt Bilgi
+            if (textBox7.Text != "")
+                graphics.DrawString(textBox7.Text, fArial8, brush, new Point(x, y + ySpaceOffset * 7));
+        }
+
+        void LoadSettings()
+        {
+            try
+            {
+                XmlTextReader xmlTReader = new XmlTextReader("settings.dat");
+                while (xmlTReader.Read())
+                {
+                    if (xmlTReader.NodeType == XmlNodeType.Element && xmlTReader.Name == "Settings")
+                    {
+                        if (xmlTReader.HasAttributes)
+                        {
+                            textBox1.Text = xmlTReader.GetAttribute("topInfo");
+                            textBox7.Text = xmlTReader.GetAttribute("bottomInfo");
+                            PrinterName = xmlTReader.GetAttribute("pName");
+                            short.TryParse(xmlTReader.GetAttribute("copies"), out printTimes);
+                            int.TryParse(xmlTReader.GetAttribute("x"), out x);
+                            int.TryParse(xmlTReader.GetAttribute("y"), out y);
+                        }
+                    }
+                }
+                xmlTReader.Close();
+            }
+            catch (Exception e) { MessageBox.Show(e.Message); }
+
+
+        }
+
+        void SaveSettings()
+        {
+            textBox8_Leave(null, null);
+            textBox9_Leave(null, null);
+
+            using (XmlTextWriter tWriter = new XmlTextWriter("settings.dat", Encoding.UTF8))
+            {
+                tWriter.WriteStartDocument();
+
+                tWriter.WriteStartElement("Settings");
+
+                tWriter.WriteAttributeString("topInfo", textBox1.Text);
+                tWriter.WriteAttributeString("bottomInfo", textBox7.Text);
+                tWriter.WriteAttributeString("pName", PrinterName);
+                tWriter.WriteAttributeString("copies", printTimes.ToString());
+                tWriter.WriteAttributeString("x", x.ToString());
+                tWriter.WriteAttributeString("y", x.ToString());
+
+                tWriter.WriteEndElement();
+
+                tWriter.WriteEndDocument();
+
+                tWriter.Close();
+            }
+
+        }
+    }
+}
